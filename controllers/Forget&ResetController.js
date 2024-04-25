62edce17942528c87016fd2d7e896210e74f8951
Devesh <devsis@gmail.com>
2024-04-25 15:56:35 +0530

    updatereset

--- controllers/Forget&ResetController.js
+++ controllers/Forget&ResetController.js
@@ -3,9 +3,8 @@
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
-const userData = require("../model/UserModel"); // Assuming you have a user model
-const Mailgen = require("mailgen"); // Import Mailgen
+const userData = require("../model/UserModel");
+const Mailgen = require("mailgen");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
@@ -20,11 +19,8 @@ const transporter = nodemailer.createTransport({
 const mailGenerator = new Mailgen({
   theme: "default",
   product: {
-    // Appears in header & footer of e-mails
     name: "YourApp",
     link: "https://yourapp.com/",
-    // Optional product logo
-    // logo: 'https://mailgen.js/img/logo.png'
   },
 });
 
@@ -45,7 +41,6 @@ const generateResetPasswordMail = (email, otp) => {
     },
   };
 
-  // Generate HTML from the template
   const emailBody = mailGenerator.generate(emailTemplate);
 
   return {
@@ -60,7 +55,6 @@ const sendMail = async (mailOptions) => {
   return transporter.sendMail(mailOptions);
 };
 
-
 const forgetPassword = async (req, res) => {
   try {
     const { email } = req.body;
@@ -68,22 +62,14 @@ const forgetPassword = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-
-    // Generate OTP
     const otp = crypto.randomInt(100000, 999999).toString();
-
-     // Hash the OTP
-     const hashedOTP = await bcrypt.hash(otp, 10);
-     const tme = new Date()
-     tme.setMinutes(tme.getMinutes()+10)
-
-
-    await user.update({ otp: hashedOTP, timeExpire: tme }); // OTP expires in 10 minutes
-
+    const hashedOTP = await bcrypt.hash(otp, 10);
+    const tme = new Date();
+    tme.setMinutes(tme.getMinutes() + 10);
+    await user.update({ otp: hashedOTP, timeExpire: tme });
     // Generate reset password email using Mailgen
     const mailOptions = generateResetPasswordMail(email, otp);
     await sendMail(mailOptions);
-
     res.json({ message: "OTP sent successfully" });
   } catch (error) {
     console.error("Error in forgetPassword:", error);
@@ -94,32 +80,22 @@ const forgetPassword = async (req, res) => {
 const resetPassword = async (req, res) => {
   try {
     const { email, password, otp } = req.body;
-
     const user = await userData.findOne({ where: { email } });
-
     if (!user) {
       return res.status(400).json({ error: "Invalid User" });
     }
-    // Check if OTP matches
+    if (user.otp === null) {
+      return res.status(403).json({ error:"Invalid OTP"})
+    }
     const otpMatch = await bcrypt.compare(otp, user.otp);
-    console.log(user.timeExpire);
-
-
-    // Check if OTP has expired
     if (otpMatch && new Date() < user.timeExpire) {
       const hashedPassword = await bcrypt.hash(password, 10);
-      console.log(hashedPassword);
-      await user.update({password: hashedPassword, otp: null,timeExpire: null});
+      await user.update({password: hashedPassword,otp: null,timeExpire: null});
       return res.status(201).json({ message: "Password Reset Successfull" });
     }
-
-    // Update user's password
-    
-
-    res.status(500).json({ error: "Invalid or Expired Otp" });
+    res.status(500).json({ error: "Invalid or Expired OTP" });
   } catch (error) {
-    console.error("Error resetting password:", error);
-    res.status(400).json({ error: "Failed to reset password" });
+    res.status(400).json({ error:error.message});
   }
 };
 
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,8 @@
 const { where } = require("sequelize");
-const crypto = require("crypto");
 const userData = require("../model/UserModel.js");
 const jwt = require("jsonwebtoken");
-const {
-  isValidEmail,
-  isValidPassword,
-  isValidName,
-} = require("../validation/Validation.js");
+const bcrypt = require('bcrypt');
+
 
 require("dotenv").config();
 const sk = process.env.SK;
@@ -15,42 +11,15 @@ const createToken = (user) => {
   return jwt.sign({ userId: user._id }, sk, { expiresIn: "1h" });
 };
 
-const hashPassword = (password) => {
-  return crypto.createHash("sha256").update(password).digest("hex");
-};
 
 const UserRegistration = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const hashedPassword = hashPassword(password);
-
-    // Validate email
-    if (!isValidEmail(email)) {
-      return res.status(400).json({ error: "Invalid email format" });
-    }
-
-    // Validate password
-    if (!isValidPassword(password)) {
-      return res
-        .status(400)
-        .json({ error: "Password should have at least 8 characters" });
-    }
-
-    // Validate name
-    if (!isValidName(name)) {
-      return res.status(400).json({ error: "Invalid name format" });
-    }
-
-    const userregi = await userData.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-    const token = createToken(userregi);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userregi = await userData.create({name,email,password: hashedPassword});
+    const token = createToken(email);
     res.cookie("token", token, { httpOnly: true });
-    res
-      .status(201)
-      .json({ token, userregi, message: res.__("create-message") });
+    res.status(201).json({ token, userregi, message: res.__("create-message") });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -58,29 +27,18 @@ const UserRegistration = async (req, res) => {
 
 const UserLogin = async (req, res) => {
   const { email, password } = req.body;
-
-  if (!isValidEmail(email)) {
-    return res.status(400).json({ error: "Invalid email format" });
-  }
-
-  if (!isValidPassword(password)) {
-    return res
-      .status(400)
-      .json({ error: "Password should have at least 8 characters" });
-  }
-
-  const hashedPassword = hashPassword(password);
   try {
-    const user = await userData.findOne({
-      where: { email, password: hashedPassword },
-    });
-    if (user && user.password === password) {
-      const token = createToken(user);
+    const user = await userData.findOne({where: { email}});
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid Password" });
+    }
+      const token = createToken(user.email);
       res.cookie("token", token, { httpOnly: true });
       res.status(200).json({ token, user, message: res.__("login-message") });
-    } else {
-      res.status(401).json({ error: "Invalid User" });
-    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -91,9 +49,7 @@ const getUserById = async (req, res) => {
     const user = await userData.findByPk(req.params.id);
     res.status(201).json({ user, message: res.__("find-message") });
   } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
+    res.status(500).json({error: error.message});
   }
 };
 
@@ -127,10 +83,4 @@ const deleteUser = async (req, res) => {
   }
 };
 
-module.exports = {
-  UserRegistration,
-  UserLogin,
-  getUserById,
-  updateUser,
-  deleteUser,
-};
+module.exports = {UserRegistration,UserLogin,getUserById,updateUser,deleteUser};
